5b72bb466742636d1e6c82e78e2fce899d91042b
Karthik Annamalaisamy <kartsamy@amazon.com>
2024-11-14 17:00:03 +0530

    extensions folder added

---
+++ saas-application/serverless-services/src/extensions/telemetry-api/python_example_telemetry_api_extension/telemetry_dispatcher.py
@@ -0,0 +1,17 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+import os
+
+from telemetry_service import (
+    log_telemetry_stream,
+)
+
+DISPATCH_MIN_BATCH_SIZE = int(os.getenv("DISPATCH_MIN_BATCH_SIZE"));
+
+
+def dispatch_telmetery(queue, force):
+    while ((not queue.empty()) and (force or queue.qsize() >= DISPATCH_MIN_BATCH_SIZE)):
+        print("[telementry_dispatcher] Dispatch telemetry data")
+        batch = queue.get_nowait()
+        log_telemetry_stream(batch)
